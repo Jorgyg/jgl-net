1641de1a1d422f47d8bc382beaf24a385cd2f7af
Jorgyg <92818340+Jorgyg@users.noreply.github.com>
2023-08-09 15:43:06 +0200

    Add files via upload

---
+++ Ex1/Ex1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleAplication5
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA MUNDO!!!");
+            Console.ReadKey();
+
+        }
+    }
+
+}
