e8d5c575c7e4e67e1c22a5381ed09a5e013aca87
Jorgyg <92818340+Jorgyg@users.noreply.github.com>
2023-08-24 18:45:56 +0200

    Add files via upload

---
+++ POO2/Ej1/POO2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace ConsoleAplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cuenta cuenta = new Cuenta("John Doe", 1000);
+            Console.WriteLine(cuenta);
+
+            cuenta.Ingresar(500);
+            Console.WriteLine("Después de ingresar 500: " + cuenta);
+
+            cuenta.Retirar(200);
+            Console.WriteLine("Después de retirar 200: " + cuenta);
+
+            cuenta.Retirar(2000);
+            Console.WriteLine("Después de retirar 2000: " + cuenta);
+
+
+
+        }
+    }
+    class Cuenta
+    {
+        // Atributos
+        private string titular;
+        private double cantidad;
+
+        // Constructor con titular obligatorio y cantidad opcional
+        public Cuenta(string titular, double cantidad = 0)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+
+        // Propiedades
+        public string Titular
+        {
+            get { return titular; }
+            set { titular = value; }
+        }
+
+        public double Cantidad
+        {
+            get { return cantidad; }
+            set { cantidad = value; }
+        }
+
+        // Método para ingresar dinero a la cuenta
+        public void Ingresar(double cantidad)
+        {
+            if (cantidad > 0)
+            {
+                this.cantidad += cantidad;
+            }
+        }
+
+        // Método para retirar dinero de la cuenta
+        public void Retirar(double cantidad)
+        {
+            if (cantidad > 0)
+            {
+                if (this.cantidad - cantidad < 0)
+                {
+                    this.cantidad = 0;
+                }
+                else
+                {
+                    this.cantidad -= cantidad;
+                }
+            }
+        }
+
+        // Método ToString para mostrar la información de la cuenta
+        public override string ToString()
+        {
+            return $"Titular: {titular}, Cantidad: {cantidad}";
+        }
+    }
+}
+
---
+++ POO2/Ej2/Ej2/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+
+namespace ConsoleAplication5
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Ingrese la edad: ");
+            int edad = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el sexo (H/M): ");
+            char sexo = char.ToUpper(Console.ReadKey().KeyChar);
+            Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Ingrese el peso en kg: ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la altura en metros: ");
+            double altura = double.Parse(Console.ReadLine());
+
+            Persona persona1 = new Persona(nombre, edad, sexo, peso, altura);
+            Persona persona2 = new Persona(nombre, edad, sexo);
+            Persona persona3 = new Persona();
+
+            Console.WriteLine("\nInformación de persona1:");
+            MostrarInformacion(persona1);
+
+            Console.WriteLine("\nInformación de persona2:");
+            MostrarInformacion(persona2);
+
+            Console.WriteLine("\nInformación de persona3:");
+            MostrarInformacion(persona3);
+        }
+
+        static void MostrarInformacion(Persona persona)
+        {
+            Console.WriteLine(persona);
+            int resultadoIMC = persona.CalcularIMC();
+
+            switch (resultadoIMC)
+            {
+                case -1:
+                    Console.WriteLine("Estado IMC: Debajo del peso ideal");
+                    break;
+                case 0:
+                    Console.WriteLine("Estado IMC: Peso ideal");
+                    break;
+                case 1:
+                    Console.WriteLine("Estado IMC: Sobrepeso");
+                    break;
+            }
+
+            Console.WriteLine($"Es mayor de edad: {persona.EsMayorDeEdad()}");
+        }
+    }
+    class Persona
+    {
+        private string nombre;
+        private int edad;
+        private string DNI;
+        private char sexo;
+        private double peso;
+        private double altura;
+
+        private const char SEXO_POR_DEFECTO = 'H';
+        private const int IMC_DEBAJO_PESO_IDEAL = -1;
+        private const int IMC_PESO_IDEAL = 0;
+        private const int IMC_SOBREPESO = 1;
+
+        public Persona()
+        {
+            nombre = "";
+            edad = 0;
+            DNI = GenerarDNI();
+            sexo = SEXO_POR_DEFECTO;
+            peso = 0;
+            altura = 0;
+        }
+
+        public Persona(string nombre, int edad, char sexo)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.DNI = GenerarDNI();
+            this.sexo = ComprobarSexo(sexo);
+            this.peso = 0;
+            this.altura = 0;
+        }
+
+        public Persona(string nombre, int edad, char sexo, double peso, double altura)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.DNI = GenerarDNI();
+            this.sexo = ComprobarSexo(sexo);
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public int CalcularIMC()
+        {
+            double imc = peso / (altura * altura);
+
+            if (imc < 20)
+            {
+                return IMC_DEBAJO_PESO_IDEAL;
+            }
+            else if (imc >= 20 && imc <= 25)
+            {
+                return IMC_PESO_IDEAL;
+            }
+            else
+            {
+                return IMC_SOBREPESO;
+            }
+        }
+
+        public bool EsMayorDeEdad()
+        {
+            return edad >= 18;
+        }
+
+        private char ComprobarSexo(char sexo)
+        {
+            if (sexo == 'H' || sexo == 'M')
+            {
+                return sexo;
+            }
+            else
+            {
+                return SEXO_POR_DEFECTO;
+            }
+        }
+
+        private string GenerarDNI()
+        {
+            Random random = new Random();
+            int numero = random.Next(10000000, 99999999);
+            string letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+            int indice = numero % 23;
+            char letra = letras[indice];
+
+            return numero.ToString() + letra;
+        }
+
+        public override string ToString()
+        {
+            return $"Nombre: {nombre}\nEdad: {edad}\nDNI: {DNI}\nSexo: {sexo}\nPeso: {peso}\nAltura: {altura}";
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+
+        public char Sexo
+        {
+            get { return sexo; }
+            set { sexo = ComprobarSexo(value); }
+        }
+
+        public double Peso
+        {
+            get { return peso; }
+            set { peso = value; }
+        }
+
+        public double Altura
+        {
+            get { return altura; }
+            set { altura = value; }
+        }
+    }
+
+}
